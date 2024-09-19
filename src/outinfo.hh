1b2290beb497ab2ebe20e3a5a8f0534cd3158715
Arendelle <2381642961@qq.com>
2024-09-19 11:49:33 +0800

    update build system

--- src/outinfo.hh
+++ src/outinfo.hh
@@ -4,17 +4,25 @@
 
 namespace fsi::outinfo {
 
-constexpr auto version = ::std::string_view{"0.1.1"};
+constexpr auto version = ::std::string_view{
+  "FileSpliter version 0.1.1 ("
+#ifdef NDEBUG
+  "release mode"
+#else
+  "debug mode"
+#endif
+  ")"
+};
 
 constexpr auto usage = ::std::string_view{
 R"(usage: FileSpliter [option] ...
 Options:
-  <file>                       Input file.
-  -h, --help                   Print this help message and exit.
-  --version                    Print version information and exit.
-  -o <dir>                     Output dir.
-  --split-size <size>          Split size.
-  --integrate <dir>            Integrate files that -o generated in <dir>.
+  <file>                 Input file.
+  -h, --help             Print this help message and exit.
+  --version              Print version information and exit.
+  -o <dir>               Output dir.
+  --split-size <size>    Split size.
+  --integrate <dir>      Integrate files that -o generated in <dir>.
 )"
 };
 
